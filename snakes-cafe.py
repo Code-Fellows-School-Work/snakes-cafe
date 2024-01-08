1b16e4361d79f1ad7d34c8640fe08e3b65b78da8
evidad <errolvidad@gmail.com>
2024-01-08 14:05:25 -0800

    added instructions for application and menu items

---
+++ snakes-cafe.py
@@ -0,0 +1,41 @@
+# Used ChatGPT to explain how to store a multiline string instead of individually using print function for each line
+
+instructions = '''
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**                                  **
+** To quit at any time, type "quit" **
+**************************************
+'''
+
+print(instructions)
+
+menu = '''
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+'''
+
+print(menu)
